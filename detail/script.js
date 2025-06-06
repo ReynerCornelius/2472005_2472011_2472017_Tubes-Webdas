8a904f45ca29df81866d59d8387147e2e68d8e4e
ReynerCornelius <2472005@maranatha.ac.id>
2025-06-06 20:02:50 +0700

    revisi navbar

---
+++ detail/script.js
@@ -0,0 +1,25 @@
+    window.addEventListener('message', function(event) {
+        if (event.data.action === 'home') {
+        window.location.href = '/';
+        }
+    });
+    window.addEventListener('message', function(event) {
+        if (event.data.action === 'resep') {
+        window.location.href = '/';
+        }
+    });
+    window.addEventListener('message', function(event) {
+        if (event.data.action === 'contact') {
+        window.location.href = '/';
+        }
+    });
+    window.addEventListener('message', function(event) {
+        if (event.data.action === 'about') {
+        window.location.href = '/aboutus/about_us.html';
+        }
+    });
+    window.addEventListener('message', function(event) {
+        if (event.data.action === 'detail') {
+        window.location.href = 'detail.html';
+        }
+    });
