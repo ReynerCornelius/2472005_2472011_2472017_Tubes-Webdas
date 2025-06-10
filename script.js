91a55b7b1ecf32887adbc3c1b4443c7961c4da82
SkyRyu57 <liuricko57@gmail.com>
2025-06-10 21:27:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
+let slideIndex = 1;
 
-function getPageParam() {
-  const params = new URLSearchParams(window.location.search);
-  return params.get("page") || "home";
+function showSlides() {
+  const slides = document.getElementsByClassName("slide-fade");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slideIndex++;
+  setTimeout(showSlides, 3000);
 }
 
-function showPage() {
-  const page = getPageParam();
-  const titleMap = { home: "Beranda", about: "Tentang Kami", contact: "Kontak" };
-  var judul = "Beranda";
-if (titleMap[page]) {
-  judul = titleMap[page];
-}
-document.title = "Food Blog | " + judul;
-
-var judulElemen = document.querySelector(".page-title");
-if (judulElemen) {
-  judulElemen.innerText = judul;
-}
-
-var kontenElemen = document.querySelector(".content");
-if (kontenElemen) {
-  kontenElemen.innerHTML = content;
-  // Membalik isi HTML
-  var isi = kontenElemen.innerHTML;
-  var isiDibalik = isi.split("").reverse().join("");
-  kontenElemen.innerHTML = isiDibalik;
-
-});
-
+window.onload = showSlides;
